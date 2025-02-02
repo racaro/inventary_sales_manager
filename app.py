99fbbf29a5efc0bb8c6dcb837d5692385af36e5c
Raúl <raulcarrasco9797@gmail.com>
2025-02-02 19:02:48 +0100

    añadir confirmacion venta

--- app.py
+++ app.py
@@ -1,62 +1,132 @@
 import streamlit as st
+from streamlit_modal import Modal
 import pandas as pd
 from datetime import datetime
 from src.utils.data_handler import ExcelHandler
 from src.utils import general_functions as gf
 
-EXCEL_FILE = "Template_Manu.xlsx"
 SHEET_NAME = "Variables template"
-excel_path = "src/data/" + EXCEL_FILE
-
+excel_path = "src/data/"
+stock_inicial = 1850
 data = ExcelHandler(excel_path, SHEET_NAME)
 st.set_page_config(layout="wide")
 st.title("Gestión de Inventario y Ventas")
-# data_frame = data.load_data()
-# df_columns_upper = [col.upper() for col in data_frame.columns]
-col1, col2, col3, col4 = st.columns([6, 6, 6, 6])
 
-with col1:
-    fecha = st.date_input("Fecha de venta", value=datetime.today())
-    producto = st.selectbox("Producto", ["", "Sembra 2023", "Otro"])
-    accion = st.selectbox("Acción", ["", "Venta", "Etiquetado", "Embotellado", "Otro"])
+# 🔄 Cargar y limpiar datos
+def load_sales():
+    return data.clean_data(data.load_data(sheet_name=SHEET_NAME))
 
-with col2:
+data_frame = load_sales()
+
+modal = Modal(key="confirm_modal", title="Confirmar datos de la venta")
+
+with st.sidebar:
+    if "fecha" not in st.session_state:
+        st.session_state["fecha"] = datetime.now()
+    if "producto" not in st.session_state:
+        st.session_state["producto"] = ""
+    if "cantidad" not in st.session_state:
+        st.session_state["cantidad"] = 1
+    if "precio_total_venta" not in st.session_state:
+        st.session_state["precio_total_venta"] = 0.0
+    if "metodo_pago" not in st.session_state:
+        st.session_state["metodo_pago"] = ""
+    if "cliente" not in st.session_state:
+        st.session_state["cliente"] = ""
+    if "observaciones" not in st.session_state:
+        st.session_state["observaciones"] = ""
+    if "accion" not in st.session_state:
+        st.session_state["accion"] = ""
+
+    fecha = st.date_input("Fecha de venta", value=datetime.now())
+    producto = st.selectbox("Producto", ["", "Sembra 2023"])
     cantidad = st.number_input("Botellas vendidas", min_value=1, step=1)
-    precio_venta = st.selectbox("Precio de venta", ["", 7.5, 9, 12, 14])
-
-with col3:
+    precio_total_venta = st.number_input("Precio total venta", min_value=0.0, step=0.1)
     metodo_pago = st.selectbox("Método de pago", ["", "Bizum", "Efectivo", "Factura", "Otro"])
     cliente = st.selectbox("Tipo de cliente", ["", "Muestra", "Distribución", "Horeca", "Amigos/Familia", "Público", "Otro"])
-
-with col4:
     observaciones = st.text_area("Observaciones")
-    precio_total = gf.calculate_total_price(cantidad, precio_venta) if precio_venta and cantidad else 0
-    st.markdown(f"**Precio total venta:** {precio_total:.2f} €")
+    accion = st.selectbox("Acción", ["", "Venta", "Etiquetado", "Embotellado", "Otro"])
 
-if st.button("Guardar venta"):
-    if not accion or not precio_venta or not producto or not cliente:
-        st.error("Por favor, completa todos los campos obligatorios.")
-    else:
-        new_sale = {
-            "Fecha": fecha,
-            "acción": accion,
-            "Producto": producto.lower(),
-            "num bot": cantidad,
-            "precio total": precio_total,
-            "precio botella": precio_total / cantidad if cantidad > 0 else 0,
-            "cliente": cliente,
-            "observaciones": observaciones,
-            "Método de pago": metodo_pago
-        }
+    if st.button("Guardar venta"):
+        if not accion or not producto or not cliente:
+            st.error("Completa todos los campos obligatorios.")
+        else:
+            st.session_state["new_sale"] = {
+                "Fecha de venta": fecha,
+                "Accion": accion,
+                "Producto": producto.lower(),
+                "Botellas vendidas": cantidad,
+                "Precio total venta": precio_total_venta,
+                "Precio botella": round(precio_total_venta / cantidad if cantidad > 0 else 0, 2),
+                "Cliente": cliente,
+                "Observaciones": observaciones,
+                "Metodo de pago": metodo_pago
+            }
+            modal.open()
 
-        data.append_sale(new_sale, sheet_name=SHEET_NAME)
-        st.success("Venta guardada correctamente")
+if modal.is_open():
+    with modal.container():
+        st.write("<h3 style='text-align:center;'>¿Los siguientes datos son correctos?</h3>", unsafe_allow_html=True)
+        st.table(pd.DataFrame([st.session_state["new_sale"]]))
 
-with st.expander("Historial de Ventas"):
-    data_frame = data.load_data()
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            if st.button("✅ Confirmar", key="confirmar"):
+                data.append_sale(data_frame, st.session_state["new_sale"], sheet_name=SHEET_NAME)
+                st.success("Guardado correctamente")
+                modal.close()
+                st.session_state["data_updated"] = True
+                st.session_state["fecha"] = datetime.now()
+                st.session_state["producto"] = ""
+                st.session_state["cantidad"] = 1
+                st.session_state["precio_total_venta"] = 0.0
+                st.session_state["metodo_pago"] = ""
+                st.session_state["cliente"] = ""
+                st.session_state["observaciones"] = ""
+                st.session_state["accion"] = ""
+                st.experimental_rerun()
+
+        with col3:
+            if st.button("❌ Cancelar", key="cancelar"):
+                st.warning("Cancelado.")
+                modal.close()
+
+if "data_updated" in st.session_state and st.session_state["data_updated"]:
+    data_frame = load_sales()
+    st.session_state["data_updated"] = False
+
+if len(data_frame):
+    stock_inicial = 1850
+    botellas_consumidas = sum(data_frame['Botellas vendidas'])
+    stock_actual = stock_inicial - botellas_consumidas
+    ventas_totales = round(sum(data_frame['Precio total venta']), 2)
+    precio_medio = round(ventas_totales / sum(data_frame['Botellas vendidas']), 2)
+
+    st.title("Visualización de Cálculos")
+    st.markdown(f"""
+        <style>
+        .box-container {{ display: flex; justify-content: space-between; }}
+        .box {{ display: flex; align-items: center; justify-content: center;
+                flex-direction: column; height: 80px; width: 150px;
+                border-radius: 10px; margin: 10px; font-size: 12px; font-weight: bold; }}
+        .box1 {{ background-color: rgba(255, 182, 193, 0.5); }}
+        .box2 {{ background-color: rgba(144, 238, 144, 0.5); }}
+        .box3 {{ background-color: rgba(135, 206, 250, 0.5); }}
+        .box4 {{ background-color: rgba(255, 215, 0, 0.5); }}
+        </style>
+        <div class="box-container">
+            <div class="box box1">Stock disponible<br><span style="font-size: 24px;">{stock_actual}</span></div>
+            <div class="box box2">Botellas vendidas<br><span style="font-size: 24px;">{botellas_consumidas}</span></div>
+            <div class="box box3">Precio medio botella<br><span style="font-size: 24px;">{precio_medio}€</span></div>
+            <div class="box box4">Beneficio bruto<br><span style="font-size: 24px;">{ventas_totales}€</span></div>
+        </div>
+    """, unsafe_allow_html=True)
+
+st.title("Historial de Ventas")
+with st.expander("Ver historial de ventas"):
     st.dataframe(data_frame, use_container_width=True)
 
 with st.expander("Resumen de Ventas"):
-    summary = data.get_summary()
+    summary = data.get_summary(data_frame)
     st.write(summary)
     st.bar_chart(summary)
--- src/utils/data_handler.py
+++ src/utils/data_handler.py
@@ -1,33 +1,70 @@
 import pandas as pd
+import os
 
 class ExcelHandler:
-    def __init__(self, file_path, sheet_name):
+    def __init__(self, data_folder, sheet_name):
         """
         Inicializa la clase con la ruta del archivo y el nombre de la hoja.
+        :param data_folder: Ruta del directorio de datos.
+        :param sheet_name: Nombre de la hoja del archivo Excel.
         """
-        self.file_path = file_path
+        self.data_folder = data_folder
         self.sheet_name = sheet_name
+        self.file_path = self._find_excel_file()
+        self.columns = ["Fecha de venta", "Accion", "Producto", "Botellas vendidas", "Precio total venta", "Precio botella", "Cliente", "Observaciones", "Metodo de pago"]
 
+    def _find_excel_file(self):
+        """
+        Busca un archivo Excel en el directorio de datos.
+        :return: Ruta del archivo Excel.
+        """
+        files = [f for f in os.listdir(self.data_folder) if f.endswith(".xlsx")]
+        if not files:
+            raise FileNotFoundError(f"No se encontró ningún archivo Excel en {self.data_folder}.")
+        return os.path.join(self.data_folder, files[0])
+    
     def load_data(self, sheet_name=None):
-        """Carga los datos desde el archivo Excel."""
+        """
+        Carga los datos desde el archivo Excel.
+        :param sheet_name: Nombre de la hoja donde se cargarán los datos.
+        :return: DataFrame con los datos cargados.
+        """
         sheet = sheet_name if sheet_name else self.sheet_name
         try:
             data = pd.read_excel(self.file_path, sheet_name=sheet)
             return data
         except FileNotFoundError:
             return pd.DataFrame()
+        
+    def clean_data(self, data):
+        """
+        Limpia los datos del archivo Excel que tengan columnas vacías a partir del tamaño de columnas.
+        :param data: DataFrame con los datos a limpiar.
+        :param columns: Lista con los nombres de las columnas.
+        :return: DataFrame con los datos limpios.
+        """
+        data_set = data.iloc[:, :len(self.columns)]
+        data_set_clean = data_set[~data_set["Accion"].isin(["Embotellado", "embotellado", "Etiquetado", "etiquetado"])]
+        return data_set_clean
 
-    def save_data(self, df, sheet_name=None):
+    def save_data(self, data, sheet_name=None):
         """
         Carga los datos desde el archivo Excel.
+        :param df: DataFrame con los datos a guardar.
+        :param sheet_name: Nombre de la hoja donde se guardarán los datos.
         :return: DataFrame con los datos cargados.
         """
         sheet = sheet_name if sheet_name else self.sheet_name
         with pd.ExcelWriter(self.file_path, engine="openpyxl", mode="a", if_sheet_exists="replace") as writer:
-            df.to_excel(writer, sheet_name=sheet, index=False)
+            data.to_excel(writer, sheet_name=sheet, index=False)
 
-    def append_sale(self, new_sale, sheet_name=None):
-        """Agrega una nueva fila (venta) al archivo Excel."""
+    def append_sale(self, data, new_sale, sheet_name=None):
+        """
+        Agrega una nueva fila (venta) al archivo Excel.
+        :param data: DataFrame con los datos actuales.
+        :param new_sale: Diccionario con los datos de la nueva venta.
+        :return: DataFrame con los datos actualizados.
+        """
         sheet = sheet_name if sheet_name else self.sheet_name
         try:
             df = self.load_data(sheet)
@@ -45,17 +82,16 @@ class ExcelHandler:
         except Exception as e:
             raise RuntimeError(f"Error al agregar una nueva venta: {e}")
 
-
-    def get_summary(self):
+    def get_summary(self, data):
         """
         Devuelve un resumen estadístico de las ventas por producto.
+        :param df: DataFrame con los datos de las ventas.
         :return: Diccionario con el resumen de ventas.
         """
         try:
-            df = self.load_data()
-            if "Producto" in df.columns:
-                df["Producto"] = df["Producto"].astype(str).str.lower()
-            summary = df.groupby("Producto")["precio total"].sum()
+            if "Producto" in data.columns:
+                data["Producto"] = data["Producto"].astype(str).str.lower()
+            summary = data.groupby("Producto")["Precio total venta"].sum()
             return summary
         except Exception as e:
             raise RuntimeError(f"Error al generar el resumen: {e}")
